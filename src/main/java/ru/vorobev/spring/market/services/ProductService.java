87bc76614cd93c5709058461774dc6bce773abc2
VorobevOleg <kukuruza-09@ya.ru>
2022-10-25 16:32:30 +0300

    Перетащено все, что делалось на уроке
    - добавлены категории
    - реализованы связи: категория - продукт

--- src/main/java/ru/vorobev/spring/market/services/ProductService.java
+++ src/main/java/ru/vorobev/spring/market/services/ProductService.java
@@ -2,34 +2,34 @@ package ru.vorobev.spring.market.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.vorobev.spring.market.dtos.CreateNewProductDto;
+import ru.vorobev.spring.market.converters.ProductConverter;
 import ru.vorobev.spring.market.dtos.ProductDto;
+import ru.vorobev.spring.market.entities.Category;
 import ru.vorobev.spring.market.entities.Product;
+import ru.vorobev.spring.market.exceptions.ResourceNotFoundException;
+import ru.vorobev.spring.market.repositories.CategoryRepository;
 import ru.vorobev.spring.market.repositories.ProductRepository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+    private final ProductConverter productConverter;
 
-    public List<ProductDto> findAll() {
-        return productRepository.findAll().stream()
-                .map(pr -> new ProductDto(pr.getId(), pr.getTitle(), pr.getPrice())).collect(Collectors.toList());
+    public List<Product> findAll() {
+        return productRepository.findAll();
     }
 
     public Optional<Product> findById(Long id) {
         return productRepository.findById(id);
     }
 
-    public void createNewProduct(CreateNewProductDto createNewProductDto) {
-        if (createNewProductDto != null) {
-            Product product = new Product();
-            product.setTitle(createNewProductDto.getTitle());
-            product.setPrice(createNewProductDto.getPrice());
+    public void createNewProduct(ProductDto productDto) {
+        if (productDto != null) {
+            Product product = productConverter.dtoToEntity(productDto);
             productRepository.save(product);
         }
     }
